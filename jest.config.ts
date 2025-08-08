f8c640b78523bc46a12801622520579e2cabf648
jawad571 <jawadmustafa571@gmail.com>
2025-08-08 21:07:33 +0100

    add coverage scripts

--- jest.config.ts
+++ jest.config.ts
@@ -6,6 +6,26 @@ const config: Config = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1', 
   },
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html', 'json'],
+  collectCoverageFrom: [
+    'app/**/*.{ts,tsx}',
+    '!app/**/*.d.ts',
+    '!app/**/node_modules/**',
+    '!app/**/*.config.{js,ts}',
+    '!app/**/coverage/**',
+    '!app/**/*.test.{ts,tsx}',
+    '!app/**/*.spec.{ts,tsx}',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50,
+    },
+  },
 };
 
 export default config;
