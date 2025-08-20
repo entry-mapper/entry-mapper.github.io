afb7a189e461730bc78f1f6ad5725f09099b8de7
Jawad Mustafa <45639306+jawad571@users.noreply.github.com>
2025-08-20 19:04:18 +0100

    Revert "47 jest remaining testcases"

--- jest.setup.ts
+++
@@ -1,9 +0,0 @@
-// jest.setup.ts
-import "@testing-library/jest-dom";
-
-// Mock the getComputedStyle method
-Object.defineProperty(window, 'getComputedStyle', {
-  value: () => ({
-    getPropertyValue: (prop: string) => '',
-  }),
-});
